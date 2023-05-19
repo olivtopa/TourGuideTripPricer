14f20196d25e979ff3d8a51cab2093071cbb662f
Olivier <amoliv_825@hotmail.com>
2023-05-19 18:15:26 +0200

    Split TourGuideTripPricer

--- src/main/java/tripPricer/contoller/TripPricerController.java
+++ src/main/java/tripPricer/contoller/TripPricerController.java
@@ -4,10 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 import tripPricer.Provider;
 import tripPricer.service.TripPricerService;
-
 
 import java.util.List;
 import java.util.UUID;
@@ -15,18 +13,19 @@ import java.util.UUID;
 @RestController
 public class TripPricerController {
 
-    @Autowired TripPricerService tripPricerService;
+    @Autowired
+    TripPricerService tripPricerService;
 
     @RequestMapping("/getPrice")
-    public List<Provider> getPrices (@RequestParam String apiKey, @RequestParam UUID attractionId,
+    public List<Provider> getPrices(@RequestParam String apiKey, @RequestParam UUID attractionId,
                                     @RequestParam int adults, @RequestParam int children, @RequestParam int nightsStay,
-                                     @RequestParam int rewardPoints){
+                                    @RequestParam int rewardPoints) {
         return tripPricerService.getPrices(apiKey, attractionId, adults, children, nightsStay, rewardPoints);
     }
 
     @RequestMapping("/getProviderName")
-    public String getProviderName (@RequestParam String apiKey, @RequestParam int adults){
-        return tripPricerService.providerName(apiKey,adults);
+    public String getProviderName(@RequestParam String apiKey, @RequestParam int adults) {
+        return tripPricerService.providerName(apiKey, adults);
     }
 
 }
--- src/main/java/tripPricer/service/TripPricerService.java
+++ src/main/java/tripPricer/service/TripPricerService.java
@@ -8,15 +8,16 @@ import java.util.List;
 import java.util.UUID;
 
 
-
 @Service
 public class TripPricerService {
     TripPricer tripPricer = new TripPricer();
-    public List<Provider> getPrices(String apiKey, UUID attractionId,int adult,int children,int nightsStays,int rewardsPoints){
 
-        return tripPricer.getPrice(apiKey, attractionId,adult,children,nightsStays,rewardsPoints);
+    public List<Provider> getPrices(String apiKey, UUID attractionId, int adult, int children, int nightsStays, int rewardsPoints) {
+
+        return tripPricer.getPrice(apiKey, attractionId, adult, children, nightsStays, rewardsPoints);
     }
+
     public String providerName(String apiKey, int adults) {
-        return tripPricer.getProviderName(apiKey,adults);
+        return tripPricer.getProviderName(apiKey, adults);
     }
 }
